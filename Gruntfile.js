86afa7429cfd9c99477bb0b1685160ee4c6b1263
Eduard Kizmann <info@vanki.de>
2015-01-24 01:05:50 +0100

    First commit

---
+++ Gruntfile.js
@@ -0,0 +1,36 @@
+module.exports = function (grunt) {
+
+// Load npm tasks
+grunt.loadNpmTasks('grunt-contrib-less');
+grunt.loadNpmTasks('grunt-contrib-watch');
+
+// Project configuration.
+grunt.initConfig({
+    less: {
+        development: {
+            files: {
+                "css/font-awesome.css": "bower_components/font-awesome/less/font-awesome.less",
+                "css/kube.css": "bower_components/kube-framework/less/kube.less",
+                "css/codenav.css": "less/codenav.less"
+            }
+        }
+    },
+    watch: {
+        css: {
+            files: [
+                "bower_components/font-awesome/less/*.less",
+                "bower_components/kube-framework/less/*.less",
+                "less/*.less"
+            ],
+            tasks: ['less'],
+            options: {
+                livereload: false,
+            }
+        }
+    }
+});
+
+// Default task(s).
+grunt.registerTask('default', ['less', 'watch']);
+
+};
