865e7d04e9c27a5f1a41f1552097140266ea360a
Eduard Kizmann <info@vanki.de>
2015-01-27 00:26:52 +0100

    Added fuzzy search

--- javascript/codenav.jquery.js
+++ javascript/codenav.jquery.js
@@ -1,5 +1,5 @@
 (function($) {
-    $.fn.codenav = function(options, lang) {
+    $.fn.codenav = function(config, lang) {
 
         // Rewrite this
         var self = this;
@@ -9,102 +9,54 @@
             "lang": "en",
             "name": "codenav",
             "version": "0.0.1",
-            "list": "file:///D:/dev/codenav/list.json",
-            "animation": {
-                "console": {
-                    "slidedown": 200,
-                    "fadein": 200,
-                    "fadeout": 400,
+            "list": [
+                {
+                    "id": "google",
+                    fn: function(value) {
+                        window.location = 'https://www.google.de/search?q=' + encodeURI(value);
+                        return;
+                    }
                 },
-                "message": {
-                    "slidedown": 400,
-                    "slideup": 800,
-                    "fadein": 400,
-                    "fadeout": 800,
-                    "delay": 3000
-                }
-            },
-            "commands": {
-                "clear": {
-                    "_fn": "fnClearExec()",
-                    "regexp": /^(clear)$/,
-                    "valid": null,
-                    "exec": null
+                {
+                    "id": "bing",
+                    fn: function(value) {
+                        window.location = 'https://www.bing.com/search?q=' + encodeURI(value);
+                        return;
+                    }
                 },
-                "version": {
-                    "_fn": "fnVersionExec()",
-                    "regexp": /^(version)$/,
-                    "valid": null,
-                    "exec": null
-                },
-                "help": {
-                    "_fn": "fnHelpExec()",
-                    "regexp": /^(help)$/,
-                    "valid": null,
-                    "exec": null
-                },
-                "echo": {
-                    "_fn": "fnEchoExec(input)",
-                    "regexp": /^(echo).*$/,
-                    "valid": null,
-                    "exec": null
-                },
-                "go": {
-                    "_fn": "fnGoExec(input)",
-                    "regexp": /^(go).*$/,
-                    "valid": null,
-                    "exec": null
-                },
-                "search": {
-                    "_fn": "fnSearchExec(input)",
-                    "regexp": /^(google|bing|npm|bower|jquery).*$/,
-                    "valid": null,
-                    "exec": null
-                }
-            }
+            ]
         };
 
-        // Define languages
-        var lang = {
-            // English
-            "en": {
-                "notfound": "the command \"{0}\" does not exist!",
-                "empty": "at least one argument is needed!",
-                "version": "version: {0}",
-                "help": "these commands are available: {0}",
-                "go_index": "these pages are available: {0}",
-                "go_notfound": "the page \"{0}\" does not exist!"
+        // Combine defaults and config
+        config = jQuery.extend(defaults, config);
+
+        var fnHelper = {
+            clear: function(element) {
+               $(self).find(element).html(null);
+               return;
             },
-            // German
-            "de": {
-                "notfound": "Der Befehl \"{0}\" existiert nicht!",
-                "empty": "Es wird mindestens ein Argument benötigt!",
-                "version": "Version: {0}",
-                "help": "Folgende Befehle sind verfügbar: {0}",
-                "go_index": "Folgende Seiten sind verfügbar: {0}",
-                "go_notfound": "Die Seite \"{0}\" existiert nicht!"
+            fadeIn: function(element, config) {
+                var config = jQuery.extend({delay: 0, fade: 400, slide: 400}, config);
+                var element = $(self).find(element);
+                element.delay(config.delay).slideDown(
+                    {queue: true, duration: config.slide}
+                ).animate(
+                    {opacity: 1}, {queue: true, duration: config.fade}
+                );
+                return;
+            },
+            fadeOut: function(element, config) {
+                var config = jQuery.extend({delay: 0, fade: 400, slide: 400}, config);
+                var element = $(self).find(element);
+                element.delay(config.delay).animate(
+                    {opacity: 0}, {queue: true, duration: config.fade}
+                ).slideUp(
+                    {queue: true, duration: config.slide}
+                );
+                return;
             }
-        };
 
-        // Combine defaults and options
-        options = $.extend(defaults, options);
 
-        // Run ajax request if list is string
-        if(typeof options.list == 'string' && options.list.match(/^(http|https|file)\:\/\/.*$/)) $.ajax(
-            { type: 'GET',  url: options.list }
-        ).success(function(res) {
-            options.list = JSON.parse(res);
-        }).fail(function() {
-            options.list = {};
-        });
-
-        // Helper to return text in the right language
-        var helperLang = function(key, vals) {
-            var string = lang[options.lang][key];
-            _.each(vals, function(val, i) {
-                string = string.replace('{' + i + '}', val);
-            });
-            return string;
         }
 
         // Helper to show a message
@@ -113,17 +65,17 @@
             $(self).find('div.cn-message').html('<div class="cn-hidden">' + _.escape(message) + '</div>');
             // Animate slidedown and fadein
             $(self).find('div.cn-message').find('div').slideDown(
-                { queue: true, duration: options.animation.message.slidedown}
+                { queue: true, duration: config.animation.message.slidedown}
             ).animate(
                 { opacity: 1 },
-                { queue: true, duration: options.animation.message.fadein }
+                { queue: true, duration: config.animation.message.fadein }
             );
             // Animate slideup and fadeout with delay
-            $(self).find('div.cn-message').find('div').delay(options.animation.message.delay).animate(
+            $(self).find('div.cn-message').find('div').delay(config.animation.message.delay).animate(
                 { opacity: 0 },
-                { queue: true, duration: options.animation.message.fadeout }
+                { queue: true, duration: config.animation.message.fadeout }
             ).slideUp(
-                { queue: true, duration: options.animation.message.slideup}
+                { queue: true, duration: config.animation.message.slideup}
             );
             return;
         };
@@ -134,145 +86,66 @@
             $(self).find('div.cn-console').prepend('<div class="cn-hidden">' + message + '</div>');
             // Animate slidedown and fadein
             $(self).find('div.cn-console').find('div').slideDown(
-                { queue: true, duration: options.animation.console.slidedown}
+                { queue: true, duration: config.animation.console.slidedown}
             ).animate(
                 { opacity: 1 },
-                { queue: true, duration: options.animation.console.fadein }
+                { queue: true, duration: config.animation.console.fadein }
             );
-            return;
-        };
-
-        // Function clear exec
-        var fnClearExec = function() {
-            // Animate fadeout
-            $(self).find('div.cn-console').find('div').animate(
-                { opacity: 0 },
-                { queue: true, duration: options.animation.console.fadeout }
-            );
-            // Clear container after queue
-             $(self).find('div.cn-console').find('div').queue(function() {
-                 $(self).find('div.cn-console').html('');
-             });
-            return;
-        };
-
-        // Function version exec
-        var fnVersionExec = function() {
-            // Display name and current version
-            helperConsole(
-                helperLang('version', [options.version])
-            );
-            return;
-        };
-
-        // Function help exec
-        var fnHelpExec = function() {
-            // Display usable commands
-            helperConsole(
-                helperLang('help', [Object.keys(options.commands).join(', ')])
-            );
-            return;
-        };
-
-        // Function echo exec
-        var fnEchoExec = function(input) {
-            // Remove command
-            query = _.escape(input.replace(/^(echo)\s*/, ''));
-            // Write in console if input is given else send message
-            if(query) helperConsole(query);
-            else helperMessage(helperLang('empty'));
-            return;
-        };
-
-        // Function go exec
-        var fnGoExec = function(input) {
-            // Strip input to query
-            var query = input.replace(/^(go)\s*/, '');
-            var success = null;
-            // Return message if go is empty
-            if(!query) {
-                var tmp = [];
-                _.each(options.list, function(item) {
-                    tmp.push(item.id);
-                });
-                helperConsole(
-                    helperLang('go_index', [tmp.join(', ')])
-                );
-                return;
-            }
-            // Check list for matches
-            _.each(options.list, function(item) {
-                if(query == item.id) {
-                    window.location = item.url;
-                    success = true;
-                }
-            });
-            // Return if success
-            if(success) return;
-            // Return message if theres no match
-            helperMessage(
-                helperLang('go_notfound', [query])
-            );
-            return;
-        };
-
-        // Function search exec
-        var fnSearchExec = function(input) {
-            // Strip input to query
-            var query = input.replace(/^(google|bing|npm|bower|jquery)\s?/, '');
-
-            if(input.match(/^(google)/)) {
-                window.location = 'https://www.google.de/search?q=' + encodeURI(query);
-            }
-
-            if(input.match(/^(bing)/)) {
-                window.location = 'https://www.bing.com/search?q=' + encodeURI(query);
-            }
-
-            if(input.match(/^(npm)/)) {
-                window.location = 'https://www.npmjs.com/search?q=' + encodeURI(query);
-            }
-
-            if(input.match(/^(bower)/)) {
-                window.location = 'http://bower.io/search/?q=' + encodeURI(query);
-            }
-
-            if(input.match(/^(jquery)/)) {
-                window.location = 'http://api.jquery.com/?s=' + encodeURI(query);
-            }
-
             return;
         };
 
         // Create Input
-        $(self).append('<div class="cn-input"></div>');
-        $(self).find('div.cn-input').append('<label for="codenav">Input</label><input type="text" id="codenav" autofocus>');
+        $(self).append('<div class="cn-input"><input type="text" id="codenav" autofocus></div>');
+
+        // Create Command
+        $(self).append('<div class="cn-items"><ul></ul></div>');
 
         // Create message
-        $(self).append('<div class="cn-message"></div>');
+        $(self).append('<div class="cn-message"><div class="cn-hidden">aasds</div></div>');
 
         // Create console
         $(self).append('<div class="cn-console"></div>');
 
+        fnHelper.fadeOut('.cn-console');
+        fnHelper.fadeIn('.cn-message div');
+        fnHelper.fadeOut('.cn-message div', {delay: 3000});
+
         // Bind change
         $(self).find('#codenav').on('change', function() {
+            $('div.cn-items ul').html('');
+            $(this).val($(this).val() + ' ');
+            console.log($(this).val());
+        });
 
-            var input = $(this).val();
-            var success = null;
+        $(self).find('#codenav').on('keydown', function(e) {
+            var keyCode = e.keyCode || e.which;
 
-            _.each(options.commands, function(config, key) {
-                if(!success && config.regexp.exec(input)) {
-                    if(typeof config._fn == 'string') eval(config._fn);
-                    if(typeof config._fn == 'function') config._fn(input);
-                    success = true;
-                }
+            if (keyCode == 9) {
+                e.preventDefault();
+
+                $('div.cn-input input').val($('div.cn-items ul li').eq(0).html()).change();
+            }
+        });
+
+        var keyuprecent = '';
+
+        $(self).find('#codenav').on('keyup', function() {
+            // define keyupinput
+            var keyupinput = $(this).val();
+            // Fire just once per keypress
+            if(keyuprecent == keyupinput) return;  keyuprecent = keyupinput;
+            // Clean list
+            $('div.cn-items ul').html('');
+            // Create fuse object
+            var fuse = new Fuse(config.cmd, { threshold: 0.4, keys: ["id"] });
+
+            _.each(fuse.search(keyupinput), function(obj, i) {
+                 $('div.cn-items ul').append('<li>' + obj.id + '</li>');
             });
 
-            $(this).val(null);
-
-            if(success) return;
-
-            helperMessage(helperLang('notfound', [input]));
+            $('div.cn-items ul li').on('click', function() {
+                $('div.cn-input input').val($(this).html()).change();
+            });
 
         });
     };
--- javascript/main.js
+++ javascript/main.js
@@ -1,1 +1,8 @@
-$('div.codenav').codenav();
+$('div.codenav').codenav(
+    {"list": [
+        {"id": "programm", "url": "http://pr0gramm.com/"},
+        {"id": "index", "url": "http://vanki.de/"},
+        {"id": "blog", "url": "http://vanki.de/"},
+        {"id": "portfolio", "url": "http://vanki.de/"}
+    ]}
+);
